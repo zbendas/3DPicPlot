52875636604afd5273099c87ca9cd6e67f29be5a
Zachariah Bendas <zbendas@yahoo.com>
2017-10-10 10:39:15 -0500

    Implement argparse
    Implements argparse, several QoL changes to structure and cleanup of
    leftovers from previous software iterations.

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import matplotlib.pyplot as plot
 from matplotlib import animation
 from mpl_toolkits.mplot3d import Axes3D
 import random
-import math
+import argparse
+import re
+import datetime
 from Pixel import Pixel
 
 
-def init_graph():
-    image = cv2.imread("./Images/A_Sunday_on_La_Grande_Jatte,_Georges_Seurat,_1884.png", 1)
+def render_graph():
+    if args.outfile is None:
+        args.outfile = './Figures/' + re.search(r"^(.:)?(.+\\)*((.+)(\..*))$", args.infile).group(4) + '.png'
+    print("Generating image from {}.".format(args.infile))
+    generation_started = datetime.datetime.now()
+    image = cv2.imread(args.infile, 1)
     image = image[:, :, ::-1]  # Reverses OpenCV2's BGR color order to more-standard RGB color order
-    pixel_list = []
-    number_of_pixels = min((image.shape[0] * image.shape[1])/30, 2000)
+    number_of_pixels = min(int((image.shape[0] * image.shape[1])*(1/3)), args.max_samples)
     random_pixel_x = random.choices(range(0, image.shape[0]), k=number_of_pixels)
     random_pixel_y = random.choices(range(0, image.shape[1]), k=number_of_pixels)
     for i, j in zip(random_pixel_x, random_pixel_y):
         pixel = Pixel(image[i, j])
-        pixel_list.append(pixel)
         graph.scatter(pixel.R, pixel.G, pixel.B, c=pixel.normalize().array(), marker="o", depthshade=False)
     graph.set_xlabel('Red Value')
     graph.set_ylabel('Green Value')
@@ -24,6 +28,9 @@ def init_graph():
     graph.set_xbound(0, 255)
     graph.set_ybound(0, 255)
     graph.set_zbound(0, 255)
+    plot.savefig(args.outfile)
+    print("Image generated and saved at {}. "
+          "This operation took {}".format(args.outfile, datetime.datetime.now() - generation_started))
 
     return figure,
 
@@ -33,12 +40,29 @@ def animate(i):
     return figure,
 
 
-def main():
-    anim = animation.FuncAnimation(figure, animate, init_func=init_graph, frames=360, interval=20, blit=True)
-    anim.save('./Figures/test_anim.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+def render_animation():
+    anim = animation.FuncAnimation(figure, animate, init_func=render_graph, frames=360, interval=20, blit=True)
+    if args.movie_outfile is None:
+        args.movie_outfile = './Figures/' + re.search(r"^(.:)?(.+\\)*((.+)(\..*))$", args.infile).group(4) + '.mp4'
+    print("Generating animation from {}".format(args.infile))
+    generation_started = datetime.datetime.now()
+    anim.save(args.movie_outfile, fps=30, extra_args=['-vcodec', 'libx264'])
+    print("Animation generated and saved at {}. "
+          "This operation took {}".format(args.movie_outfile, datetime.datetime.now() - generation_started))
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate 3D scatter plots by color-sampling images.")
+    parser.add_argument('infile', help="Path to the input image")
+    parser.add_argument('-o', '--outfile', nargs='?', help="Path to output the 3D plot")
+    parser.add_argument('-s', '--max_samples', nargs='?', help="Maximum number of sample points to take",
+                        type=int, default=5000)
+    parser.add_argument('-a', '--animate', action="store_true", help="Render an animation")
+    parser.add_argument('-m', '--movie_outfile', nargs='?', help="Path to output the animation")
+    args = parser.parse_args()
     figure = plot.figure()
     graph = Axes3D(figure)
-    main()
+    if args.animate:
+        render_animation()
+    else:
+        render_graph()
